63ada6db5f72f0ebd77c56652b717d4d03b5a8a9
Bogutlopezc10 <camilopezc@hotmail.com>
2020-06-30 17:14:25 -0500

    Add style to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaShoppingCart } from "react-icons/fa";
 import './Header.css';
 
 const Header = () =>{
     return (
         <div className="nav shadow align-items-stretch justify-content-between fixed-top">
-            <div className="col-md-12 col-lg d-flex align-items-center justify-content-center justify-content-lg-start">
+            <div className="col-auto d-flex align-items-center justify-content-start">
                 <Link to="/">
-                    Logo
+                    CampiShop
                 </Link>
             </div>
+            <nav className="col-auto px-0 menu d-flex align-items-stretch">
+                <Link className="d-flex px-3 align-items-center" to={'/categories'}>
+                    <div className="d-flex flex-column text-center">
+                        <span>Categories</span>
+                    </div>
+                </Link>
+                <Link className="d-flex px-3 align-items-center" to={'/categories'}>
+                    <div className="d-flex flex-column text-center">
+                        <FaShoppingCart />
+                    </div>
+                </Link>
+                <Link className="d-flex px-3 align-items-center" to={'/categories'}>
+                    <div className="d-flex flex-column text-center">
+                        <img src="/login.png"  width="45" height="45" className="rounded-circle shadow z-depth-0"
+                        alt="photoHeaderNotAuthenticator" /> 
+                    </div>
+                </Link>
+            </nav>
         </div>
     )
 }
