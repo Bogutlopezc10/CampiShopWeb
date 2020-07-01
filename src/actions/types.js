d2c821bc2830a4e35e55d096aee2574314f5774f
Bogutlopezc10 <camilopezc@hotmail.com>
2020-07-01 11:43:09 -0500

    Update DEVELOP_URL

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,6 +1,5 @@
 //URLs API
-export const JUAN_URL = 'http://localhost:51828/api';
-export const CAMILO_URL = 'http://localhost:51829/api';
+export const DEVELOP_URL = 'http://localhost:51828/api';
 
 // Products
 export const FETCH_PRODUCTS = 'FETCH_PRODUCTS';
--- src/apis/shop.js
+++ src/apis/shop.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-import { CAMILO_URL } from '../actions/types'
+import { DEVELOP_URL } from '../actions/types'
 
 export default axios.create({
-    baseURL: CAMILO_URL
+    baseURL: DEVELOP_URL
 })
