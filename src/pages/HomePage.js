168070bfe90f075580ea5cac8d57e8e670606908
Bogutlopezc10 <camilopezc@hotmail.com>
2020-07-23 11:54:35 -0500

    Fix some bugs in HomePage.css

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaMobileAlt , FaLaptop, FaDesktop, FaTabletAlt, FaXbox, FaPlaystation, FaAngleDoubleRight } from "react-icons/fa";
+import { FaMobileAlt, FaLaptop, FaDesktop, FaTabletAlt, FaXbox, FaPlaystation, FaAngleDoubleRight } from "react-icons/fa";
 import './HomePage.css'
 import { Link } from 'react-router-dom';
 
@@ -7,27 +7,25 @@ const HomePage = () => {
   return (
     <div className="background-home">
       <div className="bg-gradient">
-        <div className="container">
-          <div className="row main-home">
-            <div className="col-12 d-flex justify-content-center">
-              <h1>You will find the best products</h1>
-            </div>
-            <div className="icons mt-4 col-12 d-flex justify-content-center align-items-center">
-              <FaMobileAlt />
-              <FaPlaystation />
-              <FaLaptop />
-              <FaTabletAlt />
-              <FaXbox />
-              <FaDesktop />
-            </div>
-            <div className="col-12 mt-4 d-flex justify-content-center">
-              <Link to="/products" className="btn-epic">
-                <div>
-                  <span>See catalogue <FaAngleDoubleRight /></span>
-                  <span>See catalogue <FaAngleDoubleRight /></span>
-                </div>
-              </Link>
-            </div>
+        <div className="main-home">
+          <div>
+            <h1>You will find the best products</h1>
+          </div>
+          <div className="icons">
+            <FaMobileAlt />
+            <FaPlaystation />
+            <FaLaptop />
+            <FaTabletAlt />
+            <FaXbox />
+            <FaDesktop />
+          </div>
+          <div className="mt-4 d-flex justify-content-center">
+            <Link to="/products" className="btn-epic">
+              <div>
+                <span>See catalogue <FaAngleDoubleRight /></span>
+                <span>See catalogue <FaAngleDoubleRight /></span>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
