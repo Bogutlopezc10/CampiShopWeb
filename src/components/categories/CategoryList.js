9b7995f4bfe19b8e76ff72cd2aa83619c742b715
Bogutlopezc10 <camilopezc@hotmail.com>
2020-08-03 11:25:43 -0500

    Add loader in category filters

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -1,19 +1,17 @@
 import React from 'react'
 import './Category.css'
+import Loader from 'react-loader-spinner'
 import Category from '../../components/categories/Category'
 
 const CategoryList = ({ categories, loading, onChangeFilter }) => {
 
   const renderData = () => {
-    if (categories.length === 0 && !loading) {
-      return (
-        <div>No hay data</div>
-      )
-    }
     if (categories.length === 0 && loading) {
       return (
-        <div>
-          loading.......
+        <div className="container-category-loader">
+          <div className="category-loader">
+            <Loader type="Bars" color="#545b62" height={40} width={40} />
+          </div>
         </div>
       )
     }
